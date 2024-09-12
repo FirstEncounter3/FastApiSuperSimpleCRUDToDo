8ff059923e178f43905dee8420154e316cb06bbb
First_Encounter <needenergy27@gmail.com>
2024-09-12 19:09:36 +0700

    add correct status codes, used response model

--- todo.py
+++ todo.py
@@ -1,19 +1,19 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, HTTPException, status
 
-from model import ToDo, ToDoItem
+from model import ToDo, ToDoItem, ToDoItems
 
 todo_router = APIRouter()
 
 todo_list = []
 
 
-@todo_router.post("/todo")
+@todo_router.post("/todo", status_code=status.HTTP_201_CREATED)
 async def add_todo(todo: ToDo) -> dict:
     todo_list.append(todo)
     return {"message": "Todo added successfully"}
 
 
-@todo_router.get("/todo")
+@todo_router.get("/todo", response_model=ToDoItems)
 async def retrieve_todos() -> dict:
     return {"todos": todo_list}
 
@@ -25,7 +25,10 @@ async def get_single_todo(
     for todo in todo_list:
         if todo.id == todo_id:
             return {"todo": todo}
-    return {"message": "Todo not found"}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo not found",
+    )
 
 
 @todo_router.put("/todo/{todo_id}")
@@ -36,7 +39,10 @@ async def update_todo(
         if todo.id == todo_id:
             todo.item = todo_data.item
             return {"message": "Todo updated successfully"}
-    return {"message": "Todo not found"}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo not found",
+    )
 
 
 @todo_router.delete("/todo/{todo_id}")
@@ -48,8 +54,10 @@ async def delete_single_todo(
         if todo.id == todo_id:
             todo_list.pop(index)
             return {"message": "Todo deleted successfully"}
-    return {"message": "Todo not found"}
-
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo not found",
+    )
 
 @todo_router.delete("/todo")
 async def delete_all_todos() -> dict:
